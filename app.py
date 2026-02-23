31e6b874b3e4cd0080b727a6434d4a0592399dbc
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2026-02-23 12:41:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from io import StringIO
+from interpolate import *
+
+def read_data(data):
+    vetores_dados = []
+    for line in data:
+        raw_data = line.split(' ')
+        # x_data.append(float(raw_data[0]))
+        # y_data.append(float(raw_data[1]))
+        vetores_dados.append((float(raw_data[0]),float(raw_data[1])))
+
+    # vetores_dados.append(x_data)
+    # vetores_dados.append(y_data)
+    return vetores_dados
+
+st.title('Farmacos APP')
+st.subheader('Upload e visualização de PDFs')
+
+st.write('Carregue seus dados em txt')
+upload_file = st.file_uploader('Escolha seu arquivo .xy em txt')
+
+
+if upload_file is not None:
+    raw_data = StringIO(upload_file .getvalue().decode("utf-8"))
+    data = read_data(raw_data)
+    data = interpolate(data)
+    fig = plt.figure()
+    plt.plot(data[0],data[1])
+    st.pyplot(fig)
